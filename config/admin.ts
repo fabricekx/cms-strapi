0d69a1ddf42698b88d9ddb661058b9f5ca0abbbd
fabricekx <fabricekx@gmail.com>
2025-04-04 16:08:47 +0200

    correction erreur plugins.ts

--- config/admin.ts
+++ config/admin.ts
@@ -2,7 +2,7 @@ export default ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET'),
   },
-  apiToken: {
+  'api-token': {
     salt: env('API_TOKEN_SALT'),
   },
   transfer: {
--- config/plugins.ts
+++ config/plugins.ts
@@ -1,11 +1,24 @@
-module.exports = {
-    // Exemple de configuration d'un plugin
-    'users-permissions': {
-      enabled: true,
-      config: {
-        jwtSecret: process.env.JWT_SECRET || 'defaultSecret',
+module.exports = ({ env }) => {
+    console.log('ADMIN_JWT_SECRET:', env('ADMIN_JWT_SECRET')); // Vérification de la variable
+    console.log('API_TOKEN_SALT:', env('API_TOKEN_SALT')); // Vérification de la variable
+    console.log('TRANSFER_TOKEN_SALT:', env('TRANSFER_TOKEN_SALT')); // Vérification de la variable
+  
+    return {
+      auth: {
+        secret: env('ADMIN_JWT_SECRET'),
       },
-    },
-    // Autres plugins...
+      apitoken: {
+        salt: env('API_TOKEN_SALT'),
+      },
+      transfer: {
+        token: {
+          salt: env('TRANSFER_TOKEN_SALT'),
+        },
+      },
+      flags: {
+        nps: env.bool('FLAG_NPS', true),
+        promoteEE: env.bool('FLAG_PROMOTE_EE', true),
+      },
+    };
   };
   
