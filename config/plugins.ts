b83c53e4518765e77e68ced22c4c3d50c8108fdf
fabricekx <fabricekx@gmail.com>
2025-04-04 16:32:48 +0200

    encore une autre erreur

--- config/plugins.ts
+++ config/plugins.ts
@@ -2,7 +2,8 @@ module.exports = ({ env }) => {
     console.log('ADMIN_JWT_SECRET:', env('ADMIN_JWT_SECRET')); // Vérification de la variable
     console.log('API_TOKEN_SALT:', env('API_TOKEN_SALT')); // Vérification de la variable
     console.log('TRANSFER_TOKEN_SALT:', env('TRANSFER_TOKEN_SALT')); // Vérification de la variable
-  
+    console.log('JWT_SECRET:', env('JWT_SECRET')); // Vérification de la variable
+
     return {
       auth: {
         secret: env('ADMIN_JWT_SECRET'),
@@ -19,6 +20,10 @@ module.exports = ({ env }) => {
         nps: env.bool('FLAG_NPS', true),
         promoteEE: env.bool('FLAG_PROMOTE_EE', true),
       },
+      'users-permissions': { // Ajout correct du plugin avec jwtSecret
+        config: {
+          jwtSecret: env('JWT_SECRET'),
+        },
+      },
     };
-  };
-  
+};
